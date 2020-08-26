7038f87155b88cb1f90f45c92b3c25a14df91667
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-26 13:42:10 -0700

    header div container complete

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import './Navbar.css' 
+
+
+export default class NavBar extends Component {
+
+
+    render() {
+        return(
+            <div>
+                <div>
+                    <h1 className='app-name'> Start Bootstrap </h1>
+                    <div className='navbar-button-container'>
+                        <button className='nav-button'> About </button>
+                        <button className='nav-button'>  Projects </button>
+                        <button className='nav-button'> Contact </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
